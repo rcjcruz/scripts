84308a4a1fb5a432a645ffc5b0d43ae78e7aad1b
Rica Cruz <rcruz@carbon.atmosp.physics.utoronto.ca>
2020-05-14 14:56:46 -0400

    Created list with all nc files in no2 directory

--- simple_programs.py
+++ simple_programs.py
@@ -1,7 +1,16 @@
-name = input('What is your name?\n')
-print ('Hi, %s.' % name)
+# name = input('What is your name?\n')
+# print ('Hi, %s.' % name)
 
-def hello():
-    return 'Hi :)'
+# def hello():
+#     return 'Hi :)'
 
-print(hello())
+# print(hello())
+
+###### GETTING A LIST OF FILES FROM A DIRECTORY
+import glob
+
+ncfiles = []
+for file in glob.glob("/export/data/scratch/tropomi/no2/*.nc"):
+    ncfiles.append(file)
+
+print(ncfiles)
