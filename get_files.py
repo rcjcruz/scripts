a55bb52b231a10b992a6d2f0623d101264c575fe
Rica Cruz <rcruz@carbon.atmosp.physics.utoronto.ca>
2020-05-15 13:27:35 -0400

    Fixed get_files.py to clear contents of inventory.txt prior to writing

--- get_files.py
+++ get_files.py
@@ -16,17 +16,16 @@ warnings.filterwarnings("ignore", category=UserWarning)
 # Editable variables
 outputfile = "/home/rcruz/scripts/inventory.txt" # file to save the results 
 
-if os.path.getsize(outputfile) != 0:
+if os.path.getsize(outputfile) > 0:
     open(outputfile, 'w').close()
 
-else:
-    # Open the text file
-    file_object = open(outputfile, "w+")
-    # Create an empty list
-    ncfiles = []
+# Open the text file
+file_object = open(outputfile, "w+")
+# Create an empty list
+ncfiles = []
 
-    # Iterate over the files in the no2 directory and append them to the list
-    for file in glob.glob("/export/data/scratch/tropomi/no2/*.nc"):
-        file_object.write(file + "\n")
+# Iterate over the files in the no2 directory and append them to the list
+for file in glob.glob("/export/data/scratch/tropomi/no2/*.nc"):
+    file_object.write(file + "\n")
 
-    file_object.close()
+file_object.close()
